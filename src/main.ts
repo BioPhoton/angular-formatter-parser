f67640b0924869a3cf4e29ffc7a30ca7528c967e
Michael Hladky <michael@hladky.at>
2017-07-08 00:58:07 +0200

    chore(travis): updated files and folders for travis

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { environment } from './environments/environment';
 import { FormatterParserModule } from './index';
 
-if (environment.production) {
-  enableProdMode();
-}
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(FormatterParserModule);
